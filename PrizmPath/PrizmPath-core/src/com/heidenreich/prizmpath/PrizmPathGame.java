ac2cd972885d399f73c8d8092cf44f86833e1025
guitargodd97 <guitargodd97@gmail.com>
2014-04-08 21:51:41 -0400

    Wrote the skeleton code for the main game class

--- PrizmPath/PrizmPath-core/src/com/heidenreich/prizmpath/PrizmPathGame.java
+++ PrizmPath/PrizmPath-core/src/com/heidenreich/prizmpath/PrizmPathGame.java
@@ -11,24 +11,27 @@ import com.heidenreich.prizmpath.screens.SplashScreen;
 public class PrizmPathGame extends Game {
 	public static ApplicationType appType;
 	public static AssetManager assets;
-	public static boolean musicMute = false;
-	public static boolean sfxMute = false;
+	public static boolean musicMute;
+	public static boolean sfxMute;
 	public static byte[] levelData;
 	public static byte[] optionData;
 	public static Music[][] soundpacks;
 	public static Sprite[] backgrounds;
-	public static Sprite[][] colorpacks;
+	public static Sprite[][][] colorpacks;
 	public static final String log = "PrizmPath";
 	public static final String version = "Alpha 1.0";
 
+	/*
+	 * TO DO LIST: - Write loadData() - Write loadResources() - Write savaData()
+	 */
 	// Creates the game
 	public void create() {
 		// Logs the game type for control modification
 		appType = Gdx.app.getType();
 
-		//Loads data
+		// Loads data
 		loadData();
-		
+
 		// Sets up the assets
 		PrizmPathGame.setAssetManager(new AssetManager());
 		loadResources();
@@ -66,17 +69,33 @@ public class PrizmPathGame extends Game {
 
 	// Loads the resources
 	public void loadResources() {
-
+		/*
+		 * Brief Description: Loads all the resources into the AssetManager
+		 * 
+		 * NOTE - May need multiple instanes of AssetManagers for multiple file
+		 * types, such as textures and audio.
+		 */
+		Gdx.app.log(getLog(), "Resources loaded");
 	}
 
 	// Loads the data
 	public void loadData() {
-
+		/*
+		 * Brief Description: Loads the data from level.bin and options.bin into
+		 * the byte[]s levelData and option data. This data is then read into
+		 * the static variables.
+		 */
+		Gdx.app.log(getLog(), "Data loaded");
 	}
 
 	// Saves all data
 	public void saveData() {
-
+		/*
+		 * Brief Description: Before the game closes, the data is read from the
+		 * variables back into the byte[]s and then saved in the level.bin and
+		 * options.bin files.
+		 */
+		Gdx.app.log(getLog(), "Data saved");
 	}
 
 	// Gets the AssetManager
@@ -109,42 +128,65 @@ public class PrizmPathGame extends Game {
 		PrizmPathGame.sfxMute = sfxMute;
 	}
 
-	// Gets level data for determining various things
+	// Gets level data for determining various things involving the levels
 	public static byte getLevelData(int index) {
 		// Level data saved in level.bin
+		// Contains 120 level indices
+		//
+		// 0 = Not Unlocked
+		// 1 = Not Completed
+		// 2 = No Medal
+		// 3 = Bronze
+		// 4 = Silver
+		// 5 = Gold
 		return levelData[index];
 	}
 
+	// Saves a portion of level data
 	public static void setLevelData(byte data, int index) {
 		PrizmPathGame.levelData[index] = data;
 	}
 
+	// Gets a specific option data
 	public static byte getOptionData(int index) {
+		// Saved in options.bin
+		// Music 0 | 1
+		// SFX 0 | 1
+		// Color Pack 0 | 1 | 2 | 3 | 4
+		// Sound Pack 0 | 1 | 2 | 3 | 4
+		// Background 0 | 1 | 2 | 3 | 4
 		return optionData[index];
 	}
 
+	// Saves an option data
 	public static void setOptionData(byte data, int index) {
 		PrizmPathGame.optionData[index] = data;
 	}
 
+	// Gets a particular set of sounds
 	public static Music[] getSoundpack(int index) {
 		return soundpacks[index];
 	}
 
+	// Gets the background
 	public static Sprite getBackground(int index) {
 		return backgrounds[index];
 	}
 
-	public static Sprite[] getColorpack(int index) {
+	// Gets a particular set of sprites
+	public static Sprite[][] getColorpack(int index) {
 		return colorpacks[index];
 	}
 
+	// Gets the log for debugging and naming
 	public static String getLog() {
 		return log;
 	}
 
+	// Gets the version
 	public static String getVersion() {
 		return version;
 	}
 
 }
+// © Hunter Heidenreich 2014
